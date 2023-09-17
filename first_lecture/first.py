7a371befe302adeb02fd14526bba5f8fdcc18de8
misha1potapenko <misha1potapenko@yandex.ru>
2023-09-17 19:31:35 +0300

    added  two pages

--- first_lecture/first.py
+++ first_lecture/first.py
@@ -9,5 +9,18 @@ def html_index():
     return render_template('index.html')
 
 
+@app.route('/index/index.html')
+def html_index_html():
+    return render_template('index.html')
+
+@app.route('/index/for_man.html')
+def html_index_man():
+    return render_template('for_man.html')
+
+
+@app.route('/index/for_woman.html')
+def html_index_woman():
+    return render_template('for_woman.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
